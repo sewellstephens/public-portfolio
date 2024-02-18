63779b8330fdef61288cc94a4ac6a710754d7082
Sewell Stephens <sewellstephens104@gmail.com>
2024-02-17 18:34:16 -0600

    Update Tailwind CSS configuration

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,20 @@
-export const content = ['*.html',];
-export const theme = {
-  extend: {},
-};
-export const plugins = [
+module.exports = {
+content: ['./**/*.{html,njk,md}'],
+theme: {
+  extend: {
+    typography: {
+      DEFAULT: {
+        css: {
+          a: {
+            color: '#299fff',
+            textDecoration: 'underline',
+          },
+        },
+      },
+    },
+  },
+},
+plugins: [
   require('@tailwindcss/typography')
-];
-
+],
+};
